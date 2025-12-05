319ffeb3a525b2149f9661ff696c3e62257881e8
RodJustiniano <rjustiniaon.dev@gmail.com>
2025-12-04 21:02:15 -0400

    Add search with Tavily

--- main.py
+++ main.py
@@ -1,43 +1,42 @@
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 from langchain_ollama import ChatOllama
 
 load_dotenv()
 
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches over internet 
+    Args:
+        query: The query to search for
+    Returns:
+        The search result
+    """
+    print(f"Searching for {query}")
+    return tavily.search(query=query)
+
+llm = ChatOpenAI(
+    model = "gpt-4o-mini",
+    temperature=0
+)
+tools = [TavilySearch()]
+agent = create_agent(
+    model = llm,
+    tools = tools
+)
+
+
 def main():
     print("Hello from langchain-course!")
-
-    information = """
-    Mi nombre es Brais Moure, soy un informático freelance emprendedor y amante de la tecnología.
-
-    Desde 2010, he trabajado en grandes empresas del sector como Inditex, Indra o Altia. Ocupando mis 3 últimos años, antes de establecerme como freelance, como Arquitecto de Software del área de Logística-Comercial de Inditex.
-
-    En 2014 decido crear mi propia empresa de desarrollo de software, MoureDev, dado mi profundo interés por las nuevas tendencias del sector. Finalmente, en 2015, dejo mi empleo y paso a dedicarme por completo a MoureDev, orientando mi carrera al desarrollo de aplicaciones móviles.
-
-    Actualmente dirijo MoureDev, soy co-fundador de Pilbeo y asesoro y desarrollo para empresas y startups de diferentes partes del mundo.
-
-    Si estás buscando profesionalidad y dedicación, ¡cuenta conmigo!
-    """
-
-    summary_template = """
-    Give the ifnormation {information} about a person I want you to create:
-    1. A short smummary
-    2. Two interesting facts about them
-    """
-
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"],
-        template=summary_template
-    )
-
-    llm = ChatOpenAI(temperature=0, model="gpt-4o-mini")
-    # llm = ChatOllama(temperature=0, model="gemma3:270m")
-
-    chain = summary_prompt_template | llm
-    response = chain.invoke(input={"information": information})
-    print(response.content)
-
+    
+    result = agent.invoke({"messages":HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details")})
+    print(result)
 
 
 if __name__ == "__main__":
